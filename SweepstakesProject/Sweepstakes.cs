0c0df3532348c7b4484017a76af09b39f1528248
unknown <charliesather18@gmail.com>
2020-01-30 16:22:47 -0600

    Beginning approach to bonus user story which involves using MailKit to send a winning contestant an email notifying them.

--- SweepstakesProject/Sweepstakes.cs
+++ SweepstakesProject/Sweepstakes.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
 
 namespace SweepstakesProject
 {
     class Sweepstakes
     {
-        Dictionary<int,Contestant> contestants;
+        Dictionary<int, Contestant> contestants;
         string name;
         string Name
         {
             get { return name; }
         }
 
-        public Sweepstakes(string name )
+        public Sweepstakes(string name)
         {
             this.name = name;
             contestants = new Dictionary<int, Contestant>();
@@ -33,7 +36,39 @@ namespace SweepstakesProject
             List<int> keys = new List<int>();
             keys.AddRange(contestants.Keys);
 
-            return contestants[keys[rand.Next(0, keys.Count)]];
+            Contestant winner = contestants[keys[rand.Next(0, keys.Count)]];
+
+            var message = new MimeMessage();
+
+            message.From.Add(new MailboxAddress($"{name}", $"{name}@yahoo.com"));
+            message.To.Add(new MailboxAddress($"{winner.FirstName} {winner.LastName}", $"{winner.EmailAddress}"));
+            message.Subject = ("Winner winner chicken dinner");
+
+            message.Body = new TextPart("plain")
+            {
+                Text = $@"Dear {winner.FirstName} {winner.LastName},
+We are pleased to announce that you have won this year's {name} sweepstakes. Congratulations!!!"
+            };
+
+            //using (var client = new SmtpClient())
+            //{
+            //    client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+            //    client.Connect("")
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+                return winner;
         }
 
         public void PrintContestantInfo(Contestant contestant)
